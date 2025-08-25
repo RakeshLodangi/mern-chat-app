e92417e3d254d70e09481c978e366db4d0a9c88b
RakeshLodangi <161588211+RakeshLodangi@users.noreply.github.com>
2025-08-25 20:24:25 +0530

    Update routes index.js

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRoutes);
-app.use('/api/chats', chatRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
+app.use('/chats', chatRoutes);
+app.use('/messages', messageRoutes);
+app.use('/users', userRoutes);
 
 
 // MongoDB connection
